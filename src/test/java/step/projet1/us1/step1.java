8d923ca0300096c01ff23a74bec51c3cdc768140
Fetra Faneva <114089063+2shining@users.noreply.github.com>
2023-12-17 18:47:00 +0100

    Mise à jour du fichier .java

--- src/test/java/step/projet1/us1/step1.java
+++ src/test/java/step/projet1/us1/step1.java
@@ -1,75 +1,51 @@
 package step.projet1.us1;
 
-import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.AfterClass; // Ajoutez cette ligne
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.File;
-import java.io.IOException;
+public class GestionFuseauxHorairesSteps {
 
-public class step1 {
     WebDriver driver;
 
-    @Given("je suis sur la page de login")
-    public void jeSuisSurLaPageDeLogin() {
+    @Given("l'utilisateur est sur la page de gestion des fuseaux horaires")
+    public void lUtilisateurEstSurLaPageDeGestionDesFuseauxHoraires() {
         System.setProperty("webdriver.chrome.driver", "src/test/java/step/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("http://agile-web-sniffer.free.nf/");
+        driver.get("http://votre-site.com/page-de-gestion-des-fuseaux-horaires");
     }
 
-    @When("j'entre les détails de mon identité et mot de passe corrects")
-    public void jEntreLesDétailsDeMonIdentitéEtMotDePasseCorrects() throws InterruptedException {
-        driver.findElement(By.id("ut")).sendKeys("Andry");
-        driver.findElement(By.id("mtd")).sendKeys("23112020");
-
-        Thread.sleep(2000);
+    @When("l'utilisateur sélectionne l'option d'ajout d'un nouveau fuseau horaire")
+    public void lUtilisateurSelectionneOptionAjoutNouveauFuseauHoraire() {
+        driver.findElement(By.id("id-de-l-option")).click();
     }
 
-    @And("je clique sur le bouton {string}")
-    public void jeCliqueSurLeBouton(String arg0) {
-        driver.findElement(By.id("button")).click();
+    @And("l'utilisateur entre les informations du fuseau horaire personnalisé")
+    public void lUtilisateurEntreInformationsFuseauHorairePersonnalise() {
+        // Votre code pour entrer les informations (si nécessaire)
     }
 
-    @Then("je dois être redirigé vers la page d'accueil")
-    public void jeDoisÊtreRedirigéVersLaPageDAccueil() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.quit();
+    @And("l'utilisateur confirme l'ajout du fuseau horaire")
+    public void lUtilisateurConfirmeAjoutFuseauHoraire() {
+        driver.findElement(By.id("id-du-bouton-confirmer")).click();
     }
 
-    @When("j'entre des identifiants incorrects")
-    public void jEntreDesIdentifiantsIncorrects() throws InterruptedException {
-        driver.findElement(By.id("ut")).sendKeys("andryramariarison@gmaail.com");
-        driver.findElement(By.id("mtd")).sendKeys("231120200");
-
-        Thread.sleep(2000);
+    @Then("le fuseau horaire personnalisé est ajouté à la liste d'affichage")
+    public void leFuseauHorairePersonnaliseEstAjouteListeAffichage() {
+        // Votre code pour vérifier que le fuseau horaire est ajouté (si nécessaire)
     }
 
-    @Then("je dois recevoir un message indiquant {string}")
-    public void jeDoisRecevoirUnMessageIndiquant(String expectedErrorMessage) throws InterruptedException {
-        Thread.sleep(2000);
-        driver.quit();
+    @Then("un message d'erreur indiquant {string} est affiché")
+    public void unMessageErreurIndiquantEstAffiche(String expectedErrorMessage) {
+        // Votre code pour vérifier le message d'erreur (si nécessaire)
     }
 
-    // Méthode pour récupérer le nom de la classe
-    private static String getClassName() {
-        String className = Thread.currentThread().getStackTrace()[2].getClassName();
-        return className.substring(className.lastIndexOf('.') + 1);
-    }
-
-    public static File getReportDirectory() {
-        String className = getClassName();
-        return new File("target/maven-cucumber-report/" + className);
-    }
-
-    @AfterClass
-    public static void generateReports() throws IOException {
-        File reportOutputDirectory = getReportDirectory();
-        // Le reste de votre code pour la configuration et la génération de rapports
+    @Then("le fuseau horaire personnalisé n'est pas ajouté à la liste d'affichage")
+    public void leFuseauHorairePersonnaliseNestPasAjouteListeAffichage() {
+        // Votre code pour vérifier que le fuseau horaire n'est pas ajouté (si nécessaire)
     }
 }
